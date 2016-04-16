b175ea822d6ee6e3febdfd18d73f61250431fdec
Dennis de Jong <dennisdejong@student.tudelft.nl>
2016-04-16 20:49:44 +0200

    arduino FINAL

--- Arduino_Master_Code/Arduino_Master_Code.ino
+++ Arduino_Master_Code/Arduino_Master_Code.ino
@@ -8,65 +8,23 @@
 
 
 #include <Wire.h>
-#define ADDRESSCOMPASS 0x60 // Defines address of compass
+
 #include <avr/io.h>
 #include <util/delay.h>
-#define DEVICE_ADRESS   0x20
 
+#define DEVICE_ADDRESS   0x20 // Defines address
+#define ADDRESSCOMPASS 0x60 // Defines address of compass
 
 #define US_PORT PORTL
 #define US_PIN   PINL
 #define US_DDR    DDRL
-
 #define US_POS PL1      //PORTA0
-
 #define US_ERROR 0xffff
 #define US_NO_OBSTACLE 0xfffe
 
-
-uint16_t getPulseWidth()
-{
-    uint32_t i,result;
-
-    //Wait for the rising edge
-    for(i=0; i<600000; i++)
-    {
-        if(!(US_PIN & (1<<US_POS))) continue;
-        else break;
-    }
-    if(i==600000)
-        return 0xffff; //Indicates time out
-    //High Edge Found
-    //Setup Timer1
-    TCCR1A=0X00;
-    TCCR1B=(1<<CS11); //Prescaler = Fcpu/8
-    TCNT1=0x00;       //Init counter
-    //Now wait for the falling edge
-    for(i=0; i<600000; i++)
-    {
-        if(US_PIN & (1<<US_POS))
-        {
-            if(TCNT1 > 60000) break;
-            else continue;
-        }
-        else
-            break;
-    }
-    if(i==600000)
-        return 0xffff; //Indicates time out
-    //Falling edge found
-    result=TCNT1;
-    //Stop Timer
-    TCCR1B=0x00;
-    if(result > 60000)
-        return 0xfffe; //No obstacle
-    else
-        return (result>>1);
-}
-
 const int pingPin = 48; // Pin that is used for the ping sensor
 int compass = 0; // The direction of the compass in degrees
-int distance = 0; // The distance to a object in front of the RP6 in degrees
+uint8_t distance = 0; // The distance to a object in front of the RP6 in degrees
 uint16_t distanceDriven = 0; // The distance the RP6 has driven in cm
 
 // Starts the I2C and serial connections
@@ -111,49 +69,14 @@ void printStartInfo(){
   
 }
 
+
+
+// READ SENSORS
+
 // Reads each individual sensor
 void readOut(){
   readCompass();
   readPing();
-}
-
-// Checks if there is input from the serial connections and calls the functions that rely on serial input
-void input(){
-  if(Serial.available()){
-    char serialInput = Serial.read();
-    switch(serialInput) {
-      case 'i' :
-        printInfo();
-        break;
-      case 'w' :
-        sendChar(serialInput);
-        break;
-      case 'a' :
-        sendChar(serialInput);
-        break;
-      case 's' :
-        sendChar(serialInput);
-        break;
-      case 'd' :
-        sendChar(serialInput);
-        break;
-      case 'k' :
-        sendChar(serialInput);
-        break;
-      case 'm' :
-        sendChar(serialInput);
-        break;
-      case 't' :
-        sendChar(serialInput);
-        break;  
-      case 'o' :
-        sendChar(serialInput);
-        break; 
-      case 'p' :
-        sendChar(serialInput);
-        break; 
-    }
-  }    
 }
 
 // Reads the compass and puts the value in compass
@@ -186,8 +109,89 @@ void readPing() {
 
   pinMode(pingPin, INPUT);
   duration = getPulseWidth();
-
   distance = (duration / 29 / 2);
+}
+
+//  Calculates the pulse width of the incoming signal from the ping sensor
+uint16_t getPulseWidth()
+{
+    uint32_t i,result;
+
+    //Wait for the rising edge
+    for(i=0; i<600000; i++)
+    {
+        if(!(US_PIN & (1<<US_POS))) continue;
+        else break;
+    }
+    if(i==600000)
+        return 0xffff; //Indicates time out
+    //High Edge Found
+    //Setup Timer1
+    TCCR1A=0X00;
+    TCCR1B=(1<<CS11); //Prescaler = Fcpu/8
+    TCNT1=0x00;       //Init counter
+    //Now wait for the falling edge
+    for(i=0; i<600000; i++)
+    {
+        if(US_PIN & (1<<US_POS))
+        {
+            if(TCNT1 > 60000) break;
+            else continue;
+        }
+        else
+            break;
+    }
+    if(i==600000)
+        return 0xffff; //Indicates time out
+    //Falling edge found
+    result=TCNT1;
+    //Stop Timer
+    TCCR1B=0x00;
+    if(result > 60000)
+        return 0xfffe; //No obstacle
+    else
+        return (result>>1);
+}
+
+
+
+
+// INPUT
+
+// Checks if there is input from the serial connections and calls the function associated with the input
+void input(){
+  if(Serial.available()){
+    char serialInput = Serial.read();
+    switch(serialInput) {
+      case 'i' :
+        printInfo();
+        break;
+      case 'w' :
+        sendChar(serialInput);
+        break;
+      case 'a' :
+        sendChar(serialInput);
+        break;
+      case 's' :
+        sendChar(serialInput);
+        break;
+      case 'd' :
+        sendChar(serialInput);
+        break;
+      case 'k' :
+        sendChar(serialInput);
+        break;
+      case 'm' :
+        sendChar(serialInput);
+        break;  
+      case 'o' :
+        sendChar(serialInput);
+        break; 
+      case 'q' :
+        sendChar(serialInput);
+        break;
+    }
+  }    
 }
 
 // If an 'i' is put in via serial, the direction of the RP6 is printed
@@ -237,26 +241,45 @@ void printDistanceDriven() {
    Serial.println(" cm\n");
 }
 
+void sendChar(char c) {
+      byte x = c;
+      Wire.beginTransmission(DEVICE_ADDRESS);
+      Wire.write(x);             
+      Wire.endTransmission();
+}
+
+
+
+
+// SEND DATA SENSORS
+
 // Sends data from the sensors to the slave RP6
 void sendData(){
   // If the distance to an object is 8cm or closer for 100 cycles 
-  // an 'o' is sended to the slave
+  // an 'o' is sended to the slave else an 'l' is sended.
   if(distance <= 8) {
       sendChar('o');
+    } else { 
+      sendChar('l');
     }
   
   //The value of the compass in degrees is sended to the slave
   uint8_t comTemp = compass/2;
   Serial.println(comTemp);
-  Wire.beginTransmission(DEVICE_ADRESS);
+  Wire.beginTransmission(DEVICE_ADDRESS);
   Wire.write(comTemp);
   Wire.endTransmission();
 
 }
 
+
+
+
+// ASK DATA RP6
+
 // Requests distanceDriven from the slave 
 void askData(){
-  Wire.requestFrom(DEVICE_ADRESS,1);
+  Wire.requestFrom(DEVICE_ADDRESS,1);
     delay(100); 
   while (Wire.available()) { // slave may send less than requested
     uint16_t c = Wire.read(); // receive a byte as character
@@ -265,11 +288,6 @@ void askData(){
   }
 }
 
-void sendChar(char c) {
-      byte x = c;
-      Wire.beginTransmission(DEVICE_ADRESS);
-      Wire.write(x);             
-      Wire.endTransmission();
-}
+
 
 
